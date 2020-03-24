0f0217782a65074f2a0329a688b4ff2b5926526e
ptoken <42805501+ptoken@users.noreply.github.com>
2020-03-24 16:19:51 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title> 舔狗日记</title>
+		<meta name="keywords" content="舔狗日记">
+        <meta name="description" content="舔狗日记">
+        <link rel="shortcut icon" href="favicon.ico">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="static/css/main.css" />
+		<noscript><link rel="stylesheet" href="static/css/noscript.css" /></noscript>
+		<script type="text/javascript" src="https://v1.cnzz.com/z_stat.php?id=1278694248&web_id=1278694248"></script>
+	</head>
+	<body onselectstart="return false" oncontextmenu=self.event.returnValue=false> 
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="logo">
+							<span>舔狗日记</span>						</div>
+						<div class="content">
+							<div class="inner">
+								<h2><?php echo date("Y年m月d日D")?>   晴</h2>
+								<h3><script src="./api.php "></script > </h3>
+							</div>
+						</div>
+						<nav>
+							<!--<ul>
+							
+								<li><a href="./">虚位以待</a></li>
+
+								<li><a href="./">小有成就</a></li>
+								<li><a href="./">虚位以待</a></li>
+								<li><a href="./">虚位以待</a></li>
+								<li><a href="./">虚位以待</a></li>
+
+
+								<li><a href="#elements">Elements</a></li>
+							</ul>
+						</nav>-->
+					</header>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<p class="copyright">©游弋 版权所有</p>
+					</footer>
+
+			</div>
+
+		<!-- BG -->
+			<div id="bg"></div>
+
+		<!-- Scripts -->
+			<script src="static/js/jquery.min.js"></script>
+			<script src="static/js/skel.min.js"></script>
+			<script src="static/js/util.js"></script>
+			<script src="static/js/main.js"></script>
+
+	</body>
+</html>
